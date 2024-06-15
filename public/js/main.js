55e48b8635cea726e5e0293d4add95bb7c1d3115
4njiu <158364637+4njiu@users.noreply.github.com>
2024-06-14 22:06:50 -0400

    input?

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ async function getData(apikey) {
         // const apiKey = 'R0MMvEaOveYNRgbY8ry9IS0ZRoJ4rzoJEqPghguD';
         // const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
     try {
+      //APOD
         const result = await fetch(
             `https://api.nasa.gov/planetary/apod?api_key=R0MMvEaOveYNRgbY8ry9IS0ZRoJ4rzoJEqPghguD`
         );
@@ -9,8 +10,32 @@ async function getData(apikey) {
         //console.log(APOD)
         console.log(data);
         // displayNasaData(data);
+        
+        // HTML elements
+        const title = document.querySelector("#APODTitle");
+        const date = document.querySelector("#APODDate");
+        const content = document.querySelector("#APODContent");
+
+        title.innerHTML = data.title;
+        date.innerHTML = data.date;
+        
+        // Content
+        if (data.media_type === 'image') {
+            content.innerHTML = `<img src="${data.url}" alt="${data.title}" style="max-width: 100%; height: auto;">`;
+        } else if (data.media_type === 'video') {
+            content.innerHTML = `<iframe src="${data.url}" frameborder="0" allowfullscreen style="max-width: 100%; height: auto;"></iframe>`;
+        }
+        // Explanation
+        content.innerHTML += `
+        <p>${data.explanation}</p>
+        `;
+
+        // Footer
+        // const footer = document.querySelector("footer");
+
+
       } catch (error) {
-        console.error('There was an error!', error);
+        console.error('Error!', error);
       }
     }
 getData('R0MMvEaOveYNRgbY8ry9IS0ZRoJ4rzoJEqPghguD');
